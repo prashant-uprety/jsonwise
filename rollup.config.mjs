aa602d92d69fcbd7568ef77b44fc0bc42ac7d0b7
prashantUprety7 <prashantuprety90@gmail.com>
2024-05-04 20:34:18 +0545

    Add: CLI and Manipulation methods

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,12 @@
 import path from 'node:path';
 import commonjs from '@rollup/plugin-commonjs';
-import nodeExternals from 'rollup-plugin-node-externals';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import esbuild from 'rollup-plugin-esbuild';
+import nodeExternals from 'rollup-plugin-node-externals';
 
 export default {
-  input: path.join(process.cwd(), './src/index.ts'),
+  input: path.join(process.cwd(), './src/jsonwise.ts'),
   output: [
     { format: 'es', file: './dist/esm/index.mjs' },
     { format: 'cjs', file: './dist/cjs/index.cjs' },
